ba8bbb33a34ad541ed892e81b4005c56990e4a4a
estym <evann@regnault.dev>
2026-07-01 00:23:47 +0200

    Import fix

--- src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
+++ src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
@@ -18,7 +18,13 @@
 package org.keycloak.social.discord;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import org.jboss.logging.Logger;
 import org.keycloak.broker.oidc.AbstractOAuth2IdentityProvider;
 import org.keycloak.broker.oidc.mappers.AbstractJsonUserAttributeMapper;
@@ -31,29 +37,36 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.ErrorPageException;
 import org.keycloak.services.messages.Messages;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * @author <a href="mailto:wadahiro@gmail.com">Hiroyuki Wada</a>
  */
-public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<DiscordIdentityProviderConfig>
-        implements SocialIdentityProvider<DiscordIdentityProviderConfig> {
+public class DiscordIdentityProvider
+    extends AbstractOAuth2IdentityProvider<DiscordIdentityProviderConfig>
+    implements SocialIdentityProvider<DiscordIdentityProviderConfig>
+{
 
-    private static final Logger log = Logger.getLogger(DiscordIdentityProvider.class);
+    private static final Logger log = Logger.getLogger(
+        DiscordIdentityProvider.class
+    );
 
-    public static final String AUTH_URL = "https://discord.com/oauth2/authorize";
-    public static final String TOKEN_URL = "https://discord.com/api/oauth2/token";
-    public static final String PROFILE_URL = "https://discord.com/api/users/@me";
-    public static final String GROUP_URL = "https://discord.com/api/users/@me/guilds";
-    public static final String GUILD_MEMBER_URL = "https://discord.com/api/users/@me/guilds/%s/member";
+    public static final String AUTH_URL =
+        "https://discord.com/oauth2/authorize";
+    public static final String TOKEN_URL =
+        "https://discord.com/api/oauth2/token";
+    public static final String PROFILE_URL =
+        "https://discord.com/api/users/@me";
+    public static final String GROUP_URL =
+        "https://discord.com/api/users/@me/guilds";
+    public static final String GUILD_MEMBER_URL =
+        "https://discord.com/api/users/@me/guilds/%s/member";
     public static final String DEFAULT_SCOPE = "identify email";
     public static final String GUILDS_SCOPE = "guilds";
     public static final String ROLES_SCOPE = "guilds.members.read";
 
-    public DiscordIdentityProvider(KeycloakSession session, DiscordIdentityProviderConfig config) {
+    public DiscordIdentityProvider(
+        KeycloakSession session,
+        DiscordIdentityProviderConfig config
+    ) {
         super(session, config);
         config.setAuthorizationUrl(AUTH_URL);
         config.setTokenUrl(TOKEN_URL);
@@ -71,8 +84,14 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
     }
 
     @Override
-    protected BrokeredIdentityContext extractIdentityFromProfile(EventBuilder event, JsonNode profile) {
-        BrokeredIdentityContext user = new BrokeredIdentityContext(getJsonProperty(profile, "id"), getConfig());
+    protected BrokeredIdentityContext extractIdentityFromProfile(
+        EventBuilder event,
+        JsonNode profile
+    ) {
+        BrokeredIdentityContext user = new BrokeredIdentityContext(
+            getJsonProperty(profile, "id"),
+            getConfig()
+        );
 
         String username = getJsonProperty(profile, "username");
         String discriminator = getJsonProperty(profile, "discriminator");
@@ -85,47 +104,74 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
         user.setEmail(getJsonProperty(profile, "email"));
         user.setIdp(this);
 
-        AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
+        AbstractJsonUserAttributeMapper.storeUserProfileForMapper(
+            user,
+            profile,
+            getConfig().getAlias()
+        );
 
         return user;
     }
 
     @Override
-    protected BrokeredIdentityContext doGetFederatedIdentity(String accessToken) {
+    protected BrokeredIdentityContext doGetFederatedIdentity(
+        String accessToken
+    ) {
         log.debug("doGetFederatedIdentity()");
         JsonNode profile = null;
         try {
-            profile = SimpleHttp.doGet(PROFILE_URL, session).header("Authorization", "Bearer " + accessToken).asJson();
+            profile = SimpleHttp.doGet(PROFILE_URL, session)
+                .header("Authorization", "Bearer " + accessToken)
+                .asJson();
         } catch (Exception e) {
-            throw new IdentityBrokerException("Could not obtain user profile from discord.", e);
+            throw new IdentityBrokerException(
+                "Could not obtain user profile from discord.",
+                e
+            );
         }
 
         if (getConfig().hasAllowedGuilds()) {
             if (!isAllowedGuild(accessToken)) {
-                throw new ErrorPageException(session, Response.Status.FORBIDDEN, Messages.INVALID_REQUESTER);
+                throw new ErrorPageException(
+                    session,
+                    Response.Status.FORBIDDEN,
+                    Messages.INVALID_REQUESTER
+                );
             }
         }
 
         ArrayNode groups = JsonNodeFactory.instance.arrayNode();
         if (getConfig().hasMappedRoles()) {
-            Map<String, HashMap<String, String>> mappedRoles = getConfig().getMappedRolesAsMap();
+            Map<String, HashMap<String, String>> mappedRoles =
+                getConfig().getMappedRolesAsMap();
             for (String guild : mappedRoles.keySet()) {
                 JsonNode guildMember = null;
                 try {
-                    guildMember = SimpleHttp.doGet(String.format(GUILD_MEMBER_URL, guild), session).header("Authorization", "Bearer " + accessToken).asJson();
+                    guildMember = SimpleHttp.doGet(
+                        String.format(GUILD_MEMBER_URL, guild),
+                        session
+                    )
+                        .header("Authorization", "Bearer " + accessToken)
+                        .asJson();
                     for (JsonNode role : guildMember.get("roles")) {
                         String roleString = role.textValue();
                         if (mappedRoles.get(guild).containsKey(roleString)) {
-                            groups.add("discord-" + mappedRoles.get(guild).get(roleString));
+                            groups.add(
+                                "discord-" +
+                                    mappedRoles.get(guild).get(roleString)
+                            );
                         }
                     }
                 } catch (Exception e) {
-                    throw new IdentityBrokerException("Could not obtain guild member data from discord.", e);
+                    throw new IdentityBrokerException(
+                        "Could not obtain guild member data from discord.",
+                        e
+                    );
                 }
             }
         }
         if (profile instanceof ObjectNode) {
-            ((ObjectNode) profile).put("discord-groups", groups);
+            ((ObjectNode) profile).set("discord-groups", groups);
         }
 
         return extractIdentityFromProfile(null, profile);
@@ -133,7 +179,9 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
 
     protected boolean isAllowedGuild(String accessToken) {
         try {
-            JsonNode guilds = SimpleHttp.doGet(GROUP_URL, session).header("Authorization", "Bearer " + accessToken).asJson();
+            JsonNode guilds = SimpleHttp.doGet(GROUP_URL, session)
+                .header("Authorization", "Bearer " + accessToken)
+                .asJson();
             Set<String> allowedGuilds = getConfig().getAllowedGuildsAsSet();
             for (JsonNode guild : guilds) {
                 String guildId = getJsonProperty(guild, "id");
@@ -143,7 +191,10 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
             }
             return false;
         } catch (Exception e) {
-            throw new IdentityBrokerException("Could not obtain guilds the current user is a member of from discord.", e);
+            throw new IdentityBrokerException(
+                "Could not obtain guilds the current user is a member of from discord.",
+                e
+            );
         }
     }
 
